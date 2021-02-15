d56ca711709c31cba3d62692873792c1782e464d
DustyDaria <48120886+DustyDaria@users.noreply.github.com>
2021-02-15 17:51:49 +0500

    Добавлен класс Customer и контекст CustomerContext

---
+++ RequestForRepairWPF/Data/Customer.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RequestForRepairWPF.Data
+{
+    public class Customer
+    {
+        [Key]
+        public int id_user { get; set; }
+        public string user_login { get; set; }
+        public string last_name { get; set; }
+        public string name { get; set; }
+        public string middle_name { get; set; }
+        public string position { get; set; }
+        public string phone { get; set; }
+        public string roomNUMBER_URR { get; set; }
+        public string requestID_URC { get; set; }
+    }
+}
---
+++ RequestForRepairWPF/Data/CustomerContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RequestForRepairWPF.Data
+{
+    public class CustomerContext : DbContext
+    {
+        public CustomerContext() : base("DefaultConnection")
+        {
+
+        }
+        public DbSet<Customer> Customers { get; set; }
+    }
+}

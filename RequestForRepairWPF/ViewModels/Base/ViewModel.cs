60cd422faf3d59b8d862dd0dcb32e6413629b6df
DustyDaria <48120886+DustyDaria@users.noreply.github.com>
2021-02-15 15:27:28 +0500

    Добавлены директории, соотвествующие структуре MVVM

---
+++ RequestForRepairWPF/ViewModels/Base/ViewModel.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RequestForRepairWPF.ViewModels.Base
+{
+    // Базовый класс модели-представления
+    internal abstract class ViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged([CallerMemberName]string PropertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(PropertyName));
+        }
+
+        // Задача метода Set: обновлять значения свойства, для кот. определено поле, в кот. это свойство хранит данные
+        // Разрешает задачу кольцевых обновлений свойств
+        protected virtual bool Set<T>(ref T field, T value, [CallerMemberName] string PropertyName = null)
+        {
+            if (Equals(field, value)) return false;
+            field = value;
+            OnPropertyChanged(PropertyName);
+            return true;
+        }
+
+    }
+}

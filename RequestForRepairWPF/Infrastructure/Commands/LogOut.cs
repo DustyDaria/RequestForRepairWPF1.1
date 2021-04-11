00aff7d7e54f06bd7d53d2452eb47d59acb9034a
DustyDaria <48120886+DustyDaria@users.noreply.github.com>
2021-04-11 19:10:07 +0500

    Добавлена функция выхода из аккаунта (НЕОБХОДИМО ДИАЛОГОВОЕ ОКНО - ПРЕДУПРЕЖДЕНИЕ)

---
+++ RequestForRepairWPF/Infrastructure/Commands/LogOut.cs
@@ -0,0 +1,26 @@
+﻿using RequestForRepairWPF.Data;
+using RequestForRepairWPF.Infrastructure.Commands.Base;
+using RequestForRepairWPF.Services;
+using RequestForRepairWPF.Views.Pages;
+using RequestForRepairWPF.Views.Windows;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace RequestForRepairWPF.Infrastructure.Commands
+{
+    public class LogOut : Command
+    {
+        public override bool CanExecute(object parameter) => true;
+        public override void Execute(object parameter) => AppRestart();
+
+        private void AppRestart()
+        {
+            System.Windows.Forms.Application.Restart();
+            System.Windows.Application.Current.Shutdown();
+        }
+    }
+}
